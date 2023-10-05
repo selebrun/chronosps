5ef35079ee87e6a9c2498f4ce074139be707f9af
Rogelio Mármol <rwmarmol@gmail.com>
2023-10-04 21:47:53 -0300

    Initial Odoo Config (#5)

---
+++ config/params.ts
@@ -0,0 +1,43 @@
+export const config = {
+    "companies": [
+        {
+            "id": "7ac7aa42-008b-11ee-be56-0242ac120002",
+            "company": "pkannoa",
+            "odoo_connection": {
+                "domain": "kannoa-new-test-9125248.dev.odoo.com",
+                "url": "https://kannoa-new-test-9125248.dev.odoo.com/",
+                "port": 443,
+                "db": "kannoa-new-test-9125248",
+                "username": "admin",
+                "password": "f398f1a5e37c9ffe982e39e5fe29949419630505"
+            }
+        },
+        {
+            "id": "889cd134-00b2-11ee-be56-0242ac120002",
+            "company": "pdemo",
+            "odoo_connection": {
+                "domain": "demol.chronosps.com",
+                "url": "https://demol.chronosps.com/",
+                "port": 443,
+                "db": "demo",
+                "username": "admin",
+                "password": "sim9096"
+            }
+        },
+        {
+            "id": "98710152-9f8f-465c-85ce-6bdfa7d3c467",
+            "company": "phidrosumi",
+            "odoo_connection": {
+                "domain": "hidrosumi.chronosps.com",
+                "url": "https://hidrosumi.chronosps.com/",
+                "port": 443,
+                "db": "hidrosumi-prod-4816358",
+                "username": "admin",
+                "password": "6cb4a0da4cf89b3eb60db6f1fc2cbd045c0560c3"
+            }
+        }
+    ],
+    "web_app": {
+        "port": 5680
+    }
+}
