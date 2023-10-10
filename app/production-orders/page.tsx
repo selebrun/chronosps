97bdd86a607fc352ea2899db68ca27314dc618fe
Rogelio Marmol <rwmarmol@gmail.com>
2023-10-10 17:57:49 -0300

    Update orders

--- app/production-orders/page.tsx
+++ app/production-orders/page.tsx
@@ -1,21 +1,19 @@
 import Link from 'next/link';
 import { getOrders, getProductionOrders } from '@/app/api/orders/getOrders';
-// import { getOdooData } from '@/app/api/odoo/odooService';
-
 
 const testUser = {
-  name: 'Alfonso Schiavino',
-  email: 'alfonso@chronosps.com',
-  mobile: '+56 9 3431 2199',
-  password: 'f05c9eb802ba34adc664326a72a27f8b',
-  odoo_id: 7,
-  odoo_user_id: 6,
-  document: '23400447-6',
+  name: 'Simon Lebrun Sposito',
+  email: 'simon@chronosps.com',
+  mobile: 'false',
+  password: 'ffffe4c9c6ef337d5a9b60d6423782a1',
+  odoo_id: 686,
+  odoo_user_id: 0,
+  document: '10203040',
   role: 'Operario',
-  hash: 'f47a8632fb94c9f081fe429c33136b39f29153b7698e22d7a32d3ffb597280f0',
+  hash: 'fbebfaed3f02007236b5075c64b5d8e2392ed46e93d53859e83f3e5df8d8bfef',
   active: true,
 }
-const testCompanyId = "889cd134-00b2-11ee-be56-0242ac120002";
+const testCompanyId = "7ac7aa42-008b-11ee-be56-0242ac120002";//"889cd134-00b2-11ee-be56-0242ac120002";
 
 
 
@@ -44,121 +42,52 @@ export default async function Page() {
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 relative overflow-y-auto">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                    <th scope="col" className="px-6 py-3">
-                      ID
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Nombre
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Estado
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Cantidad
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Cantidad Producida
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Inicio programado
-                    </th>
-                    <th scope="col" className="px-6 py-3"></th>
+                  <th scope="col" className="px-6 py-3">
+                    Nombre
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Estado
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Cantidad
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Lote
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Responsable
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Inicio programado
+                  </th>
+                  <th scope="col" className="px-6 py-3"></th>
                 </tr>
             </thead>
             <tbody>
               {odooOrders?.data?.map((order: any) => (
                 <tr key={`production-order-${order.id}`} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <td className="px-6 py-4">
-                      {order.id}
-                    </td>
-                    <th scope="row" className="px-5 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    <div className="flex items-center space-x-4">
-                        <div className="font-medium dark:text-white">
-                            <div>{order.product_id[1]}</div>
-                            <div className="text-sm text-gray-500 dark:text-gray-400">{order.name}</div>
-                        </div>
-                    </div>
+                    <th scope="row" className="px-5 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-5">
+                      <div className="flex items-center space-x-4 w-60 whitespace-normal">
+                          <div className="font-medium dark:text-white">
+                              <div className="text-sm text-gray-500 dark:text-gray-400">{order.name}</div>
+                              <div>{order.product_id[1]}</div>
+                          </div>
+                      </div>
                     </th>
                     <td className="px-6 py-4">
                       <span className="bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">{order.state}</span>
                     </td>
                     <td className="px-6 py-4">
-                      {order.product_qty}
+                      {order.qty_producing}/{order.product_qty}
                     </td>
                     <td className="px-6 py-4">
-                      {order.qty_producing}
+                      {order.lot_producing_id[1]}
+                    </td>
+                    <td className="px-6 py-4">
+                      {order.user_id[1]}
                     </td>
                     <td className="px-6 py-4">
                       {order.date_planned_start}
-                    </td>
-                    <td className="px-6 py-4">
-                    <Link
-                      href={`production-orders/${order.id}`}
-                      className="text-white bg-blue-800 hover:bg-blue-900 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
-                    >
-                      Detalle
-                    </Link>
-                    </td>
-                </tr>
-                ))}
-            </tbody>
-        </table>
-      </div>
-
-
-      <div className="relative overflow-y-auto h-fit">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 relative overflow-y-auto">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                    <th scope="col" className="px-6 py-3">
-                      NO. OT
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Estado
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Nombre
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Producción
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Cenro de trabajo
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Inicio programado
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                        
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {orders.map((order: any) => (
-                <tr key={order.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" className="px-5 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    <div className="flex items-center space-x-4">
-                        <img className="w-10 h-10 rounded-full" src={order.image} alt=""/>
-                        <div className="font-medium dark:text-white">
-                            <div>{order.name}</div>
-                            <div className="text-sm text-gray-500 dark:text-gray-400">{order.gender}</div>
-                        </div>
-                    </div>
-                    </th>
-                    <td className="px-6 py-4">
-                      {order.species}
-                    </td>
-                    <td className="px-6 py-4">
-                      <span className="bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">{order.status}</span>
-                    </td>
-                    <td className="px-6 py-4">
-                      {order.origin.name}
-                    </td>
-                    <td className="px-6 py-4">
-                      {order.location.name}
-                    </td>
-                    <td className="px-6 py-4">
-                      {order.created}
                     </td>
                     <td className="px-6 py-4">
                     <Link
