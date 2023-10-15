cb55037c61826776fd110c8175464fc07567c477
Arthuro <arthuro@cuadra.cl>
2023-10-15 11:15:35 -0300

    Validating that the user is logged in, if not redirect to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Image from 'next/image'
 
-function Signin() {
+function SignIn() {
   const [error, setError] = useState("");
   const router = useRouter();
 
@@ -83,4 +83,4 @@ function Signin() {
   );
 }
 
-export default Signin;
+export default SignIn;
