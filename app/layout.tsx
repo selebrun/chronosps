a5f0053bf5041159f3620e9bfb7a0b6dee01ff45
Arthuro <arthuro@cuadra.cl>
2023-10-17 00:36:08 -0300

    Added icons to password input show/hide values and showing login page in routes login and /

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,24 @@
-import './globals.css'
 import type { Metadata } from 'next'
-
-// Ui Components
-import { BreadcrumbsBar } from '@/ui/breadcrumbs-bar/breadcrumbs-bar';
-import { DashboardNav } from '@/ui/dashboard-nav/dashboard-nav';
-
+import './globals.css'
 // Providers
 import AuthProvider from './context/AuthProvider'
+import { useRouter } from 'next/navigation'
 
 export const metadata: Metadata = {
   title: 'Chronos Piso App',
   description: '',
 }
 
-export default async function RootLayout({
+export default function Layout({
   children,
 }: {
   children: React.ReactNode
 }) {
-
+  
   return (
     <html lang="en" className="[color-scheme:light]">
       <body >
-      <AuthProvider>
+        <AuthProvider>
           <div className="main">
             <div >{children}</div>
           </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-import { navItems } from '@/config/nav-links';
-import { options } from "./auth/[...nextauth]/options"
-import { getServerSession } from "next-auth/next"
-import Link from 'next/link';
-import Login from './login/page';
+"use client";
+import SignIn from "./login/SignIn";
 
-export default async function Page() {
-//   const session = await getServerSession(options)
-// console.log(session, 'session')
+export default function Page() {
   return (
-    <div className="space-y-8">
-    </div>
+    <SignIn></SignIn>
   );
 }
