e12db4160e62a634c113b6eff1877bc1f1b9452b
Verioska Ramirez <verioska.ramirez@femsasalud.com>
2023-10-23 14:37:05 -0300

    feat: update login

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -20,9 +20,6 @@ export default async function Layout({
       <div className="h-screen bg-cover bg-right bg-[url('../public/fondo_engranajes.jpg')]">
         <LogoMenu></LogoMenu>
         <div className="mx-auto max-w-7xl space-y-8 px-2 pt-20 lg:px-8 lg:py-8">
-          {/* <div className="bg-white rounded-lg p-px shadow-lg shadow-black/20">
-            <BreadcrumbsBar />
-          </div> */}
           <div className="bg-white rounded-lg p-px shadow-lg shadow-black/20">
             <div className="rounded-lg p-3.5 lg:p-6">{children}</div>
           </div>
--- app/login/SignIn.tsx
+++ app/login/SignIn.tsx
@@ -11,6 +11,7 @@ function SignIn() {
   const router = useRouter();
   const [showPassword, setShowPassword] = useState(false);
   const [companyId, setCompanyId] = useState<string | null>();
+  const [loading, setLoading] = useState<boolean>(false);
   const [formData, setFormData] = useState({
     dniUser: "",
     password: "",
@@ -38,20 +39,30 @@ function SignIn() {
 
   const handleSubmit = async (event: { preventDefault: () => void; }) => {
     event.preventDefault();
-
+    setLoading(true)
     if (!companyId) {
+      setLoading(false)
       setError('No es posible determinar la compañia, debe especificar el companyId en la url');
       return;
     }
-
+    const dni = formData.dniUser.replace(/[^\w\s]/gi, '')
     const res = await signIn("credentials", {
-      username: formData.dniUser,
+      username: dni,
       password: formData.password,
       company_id: companyId,
       redirect: false,
     });
+ 
+    if (res?.error === 'CredentialsSignin') {
+      setLoading(false)
+      setError('Usuario o contraseña incorrectas')
+      return
+    }
 
-    if (res?.error) setError(res.error);
+    if (res?.error)  {
+      setLoading(false)
+      setError(res.error);
+    }
 
     if (res?.ok) return router.push("/dashboard");
   };
@@ -74,7 +85,7 @@ function SignIn() {
 
         <form
           onSubmit={handleSubmit}
-          className="rounded m-3 px-8 pt-5 mb-4 bg-white"
+          className="rounded m-3 px-8 pt-5 mb-4 bg-white h-[250px]"
         >
           <div className="mb-4">
             <label
@@ -120,8 +131,14 @@ function SignIn() {
               </div>
             </div>
           </div>
-          <button className="bg-[#6BB2D7] font-bold shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline">
-            Ingresar
+          <button disabled={(formData.dniUser === '') || (formData.password === '')} className="bg-[#6BB2D7] flex justify-center  font-bold shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline">
+            <div>Ingresar</div>
+            <div role="status"  className='relative left-4' >
+              {loading && <svg width="20" height="20" fill="currentColor" className="mr-2 animate-spin" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                <path d="M526 1394q0 53-37.5 90.5t-90.5 37.5q-52 0-90-38t-38-90q0-53 37.5-90.5t90.5-37.5 90.5 37.5 37.5 90.5zm498 206q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-704-704q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm1202 498q0 52-38 90t-90 38q-53 0-90.5-37.5t-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-964-996q0 66-47 113t-113 47-113-47-47-113 47-113 113-47 113 47 47 113zm1170 498q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-640-704q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm530 206q0 93-66 158.5t-158 65.5q-93 0-158.5-65.5t-65.5-158.5q0-92 65.5-158t158.5-66q92 0 158 66t66 158z">
+                </path>
+              </svg>}
+            </div>
           </button>
         </form>
       </div>
