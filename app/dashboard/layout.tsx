fa1e5a0ebd17a27341c3f8a99f457d43110759e0
Rogelio Marmol <rwmarmol@gmail.com>
2023-11-02 14:34:07 -0300

    Protect Dashboard Route - Contants file added

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from 'next'
-import Image from "next/image";
+import { redirect } from 'next/navigation'
+import { getServerSession } from 'next-auth'
+import { config } from '@/auth'
+
+// Config
+import { LOGIN_URL } from '@/config/constants'
 
 // Ui Components
 import LogoMenu from './LogoMenu';
@@ -14,16 +19,18 @@ export default async function Layout({
 }: {
   children: React.ReactNode;  
 }) {
+
+  const session = await getServerSession(config)
+  if (!session) redirect(LOGIN_URL)
+
   return (
-    <>
-      <div className="h-screen bg-cover bg-right bg-[url('../public/fondo_engranajes.jpg')]">
-        <LogoMenu></LogoMenu>
-        <div className="mx-auto max-w-7xl space-y-8 px-2 pt-20 lg:px-8 lg:py-8">
-          <div className="bg-white rounded-lg p-px shadow-lg shadow-black/20">
-            <div className="rounded-lg p-3.5 lg:p-6">{children}</div>
-          </div>
+    <div className="h-screen bg-cover bg-right bg-[url('../public/fondo_engranajes.jpg')]">
+      <LogoMenu></LogoMenu>
+      <div className="mx-auto max-w-7xl space-y-8 px-2 pt-20 lg:px-8 lg:py-8">
+        <div className="bg-white rounded-lg p-px shadow-lg shadow-black/20">
+          <div className="rounded-lg p-3.5 lg:p-6">{children}</div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
