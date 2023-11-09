57868b5afa84f1566ddc3573e698f1b086e62e0f
Rogelio Marmol <rwmarmol@gmail.com>
2023-11-09 19:48:19 -0300

    Add Modal

---
+++ ui/modal/modal.tsx
@@ -0,0 +1,69 @@
+"use client"
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+
+export function Modal({ children, title, onClose, setOpen, className }: {
+  children: React.ReactNode
+  title?: string 
+  onClose?: Function
+  setOpen: boolean
+  className?: string
+}) {
+
+  function closeModal() {
+    if (onClose) onClose()
+  }
+
+  function openModal() {
+    
+  }
+
+  return (
+    <>
+      <Transition appear show={setOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel
+                  className={`w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all ${className}`}
+                >
+                  
+                  {title && (
+                    <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                      {title}
+                    </Dialog.Title>
+                  )}
+                  <div className="mt-2">
+                    {children}
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
+}
