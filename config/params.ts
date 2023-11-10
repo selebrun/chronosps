407de995702b305a67d7b821fd357465b892c045
Verioska Ramirez <verioska.ramirez@femsasalud.com>
2023-11-10 16:19:12 -0300

    feat: add work orden

--- config/params.ts
+++ config/params.ts
@@ -4,10 +4,10 @@ export const config = {
             "id": "7ac7aa42-008b-11ee-be56-0242ac120002",
             "company": "pkannoa",
             "odoo_connection": {
-                "domain": "kannoa-new-test-9125248.dev.odoo.com",
-                "url": "https://kannoa-new-test-9125248.dev.odoo.com/",
+                "domain": "kannoa-new-test-10442236.dev.odoo.com",
+                "url": "https://kannoa-new-test-10442236.dev.odoo.com/",
                 "port": 443,
-                "db": "kannoa-new-test-9125248",
+                "db": "kannoa-new-test-10442236",
                 "username": "admin",
                 "password": "f398f1a5e37c9ffe982e39e5fe29949419630505"
             }
@@ -35,6 +35,18 @@ export const config = {
                 "username": "admin",
                 "password": "6cb4a0da4cf89b3eb60db6f1fc2cbd045c0560c3"
             }
+        },
+        {
+            "id": "889cd134-00b2-11ee-be56-0242ac120002",
+            "company": "pdemo",
+            "odoo_connection": {
+                "domain": "demol6.chronosps.com",
+                "url": "https://demol6.chronosps.com/",
+                "port": 443,
+                "db": "Demo16",
+                "username": "admin",
+                "password": "d66e15abc634f18b6a8464839ba48ec22f9e53de"
+            },
         }
     ],
     "web_app": {
