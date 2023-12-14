a7e3c23e96a28b468ae4dc0d1cdae060dac20f5a
Johan Ramirez <76572167+jramirez0202@users.noreply.github.com>
2023-12-14 17:56:18 -0300

    ADD show userName in dropdown Button (#28)
    * ADD show userName in dropdown Button
    
    * FiX responsive show userName in dropdown Button
    
    * feat: add iframe
    
    ---------
    
    Co-authored-by: Verioska Ramirez <verioska.ramirez@femsasalud.com>

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -27,7 +27,9 @@ export default async function Layout({
 
   return (
     <div className="h-screen bg-cover bg-right bg-[url('../public/fondo_engranajes.jpg')]">
-      <LogoMenu></LogoMenu>
+      <LogoMenu
+        userRole={session.user.name}
+      />
       <HeaderLink userRole={session.user.role}></HeaderLink>
         <div className="mx-auto max-w-7xl space-y-8 px-2 pt-20 lg:px-8 lg:py-8">
           <div className="bg-white rounded-lg p-px shadow-lg shadow-black/20">
