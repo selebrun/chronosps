020e8d65e89b955ab64c1a86e8b2fc5dd5c35419
Verioska Ramirez <verioska.ramirez@femsasalud.com>
2024-06-13 17:26:29 -0400

    ok

--- auth.ts
+++ auth.ts
@@ -6,7 +6,7 @@ import { removeSpecialCharacters } from "@/helper/removeSpecialCharacters";
 
 //Services
 //import { getUsersFromOdoo } from '@/app/api/odoo/odooUsers';
-import { getUsers } from "./app/api/users/users";
+import { getUsers, getEmployee } from "./app/api/users/users";
 import { getOdooData } from "@/app/api/odoo/odooService";
 
 
@@ -51,22 +51,30 @@ export const config = {
        
           const user = chronosUsers.find((user: any) => removeSpecialCharacters(user.code).trim() === username && user.password.trim() === password );      
           if (!user) return null;
-          let odoo_user_id = 0
-          getOdooData('res.users',[['partner_id','=',user.code],['active','=',true]],['id'],false, false, user.id_company, (odoo_user: any) => {
-            if( odoo_user && odoo_user.data && odoo_user.data[0] && odoo_user.data[0].id) odoo_user_id = odoo_user.data[0].id
-          })
-
+          let odoo_user_id = 0;
+          await new Promise<void>((resolve, reject) => {
+              getOdooData('hr.employee', [], ['id', 'name', 'job_id', 'work_email', 'identification_id'], false, false, user.id_company, (odoo_user: any) => {
+                  if (odoo_user && odoo_user?.data?.length) {
+                      const userOdoo = odoo_user.data.find((item: any) => item.identification_id === user?.code.trim());
+                      if (userOdoo) {
+                          odoo_user_id = userOdoo.id;
+                      }
+                  }
+                  resolve();
+              });
+          });
+          
           const user_data = {
             name: user?.name?.trim(),
             email: user?.email?.trim(),
             company_id: user?.id_company?.trim(),
-            odoo_id: user?.id,
+            odoo_id:  odoo_user_id,
             odoo_user_id: odoo_user_id,
             document: user?.code?.trim(),
             role: user?.rol?.trim(),
-            materiales: user?.x_studio_new_material?.trim(),
+            materiales: user?.x_studio_new_material,
           }
-
+      
           return user_data as any;
   
         },
