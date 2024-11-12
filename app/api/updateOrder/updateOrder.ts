84416a0820db3e629122e312138e44bdb5a6a803
Verioska Ramirez <verioska.ramirez@femsasalud.com>
2024-11-12 20:25:36 -0300

    update

--- app/api/updateOrder/updateOrder.ts
+++ app/api/updateOrder/updateOrder.ts
@@ -7,10 +7,15 @@ export async function updateOrder (user: any, workorder: any, action: string, bl
     return new Promise(async (resolve, reject) => {
       getOdooData(
         'mrp.workorder',
-        [['id','=', workorder.id]],
-        ['id','name','state','production_id','duration','duration_expected','operation_note','working_state','workcenter_id','is_user_working', 'employee_assigned_ids'],
+        [['employee_assigned_ids','=',user.odoo_id], ['state', 'in', ['pending', 'waiting', 'ready', 'progress']]],
+        ['id', 'state', 'production_id'],
         false,
         false,
+        // 'mrp.workorder',
+        // [['id','=', workorder.id]],
+        // ['id','name','state','production_id','duration','duration_expected','operation_note','working_state','workcenter_id','is_user_working', 'employee_assigned_ids'],
+        // false,
+        // false,
         user.company_id,
         async (workorders: any) => {
           const work_order = workorders.data[0]
@@ -45,16 +50,16 @@ export async function updateOrder (user: any, workorder: any, action: string, bl
           }
 
           const blockReason = block_reason === false ?   false : parseInt(block_reason)
-            createOdooData('x_acciones_remotas', 
-            {x_studio_ejecutado_por: user.odoo_id, 
-              x_studio_workorder_id: workorder.id, 
-              x_studio_production: workorder.production_id[0], 
-              x_studio_accion_a_ejecutar: action, 
-              x_studio_motivo_del_bloqueo: blockReason}, user.company_id, (data: any) => {
+            // createOdooData('x_acciones_remotas', 
+            // {x_studio_ejecutado_por: user.odoo_id, 
+            //   x_studio_workorder_id: workorder.id, 
+            //   x_studio_production: workorder.production_id[0], 
+            //   x_studio_accion_a_ejecutar: action, 
+            //   x_studio_motivo_del_bloqueo: blockReason}, user.company_id, (data: any) => {
 
-                if(!data || !data?.status) return resolve({status: false, message: "Ocurrio un error al intentar ejecutar accion en Odoo."})
-              return resolve({status: true, message: "Accion realizada con exito."})
-            })
+            //     if(!data || !data?.status) return resolve({status: false, message: "Ocurrio un error al intentar ejecutar accion en Odoo."})
+            //   return resolve({status: true, message: "Accion realizada con exito."})
+            // })
 
         })
     })
