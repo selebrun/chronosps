a225d385507d30c2a8662fda3adee852426900c5
Verioska Ramirez <verioska.ramirez@femsasalud.com>
2024-11-12 17:07:54 -0300

    ok

--- app/api/updateOrder/updateOrder.ts
+++ app/api/updateOrder/updateOrder.ts
@@ -51,16 +51,16 @@ export async function updateOrder (user: any, workorder: any, action: string, bl
         }
 
         const blockReason = block_reason === false ?   false : parseInt(block_reason)
-          createOdooData('x_acciones_remotas', 
-          {x_studio_ejecutado_por: user.odoo_id, 
-            x_studio_workorder_id: workorder.id, 
-            x_studio_production: workorder.production_id[0], 
-            x_studio_accion_a_ejecutar: action, 
-            x_studio_motivo_del_bloqueo: blockReason}, user.company_id, (data: any) => {
+          // createOdooData('x_acciones_remotas', 
+          // {x_studio_ejecutado_por: user.odoo_id, 
+          //   x_studio_workorder_id: workorder.id, 
+          //   x_studio_production: workorder.production_id[0], 
+          //   x_studio_accion_a_ejecutar: action, 
+          //   x_studio_motivo_del_bloqueo: blockReason}, user.company_id, (data: any) => {
 
-              if(!data || !data?.status) return resolve({status: false, message: "Ocurrio un error al intentar ejecutar accion en Odoo."})
-             return resolve({status: true, message: "Accion realizada con exito."})
-          })
+          //     if(!data || !data?.status) return resolve({status: false, message: "Ocurrio un error al intentar ejecutar accion en Odoo."})
+          //    return resolve({status: true, message: "Accion realizada con exito."})
+          // })
 
       })
   })
