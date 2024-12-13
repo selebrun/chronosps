6fac6628ea47caf4b104b5e3149f7bbaf34e0b2e
Verioska Ramirez <verioska.ramirez@femsasalud.com>
2024-12-13 09:51:29 -0300

    update nextconfig

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedForwardedHosts: ['localhost'],
+      allowedOrigins: [
+        'http://localhost',
+        'https://piso.chronosps.app',
+      ]
+    }
+  }
+}
 
 module.exports = nextConfig
