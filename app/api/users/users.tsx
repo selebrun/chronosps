7e09c52ba204d465636efd7da630cb5fb3a9860f
winston142O <winston1420@hotmail.com>
2026-01-08 19:25:09 -0400

    Arreglar error de conexion a la base de datos

--- app/api/users/users.tsx
+++ app/api/users/users.tsx
@@ -9,8 +9,10 @@ const config = {
   host: process.env.CHRONOS_DB_HOST || "",
   database: process.env.CHRONOS_DB_NAME || "",
   password: process.env.CHRONOS_DB_PASSWORD || "",
-  port: process.env.CHRONOS_DB_PORT || "",
-  ssl: true,
+  port: process.env.CHRONOS_DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 } as any;
 
 // ------------------ HELPERS ------------------
